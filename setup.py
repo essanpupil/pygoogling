5a303bc30ac6af3e7477e45fc504de31335499d3
Ikhsan Noor Rosyidin <jakethitam1985@gmail.com>
2016-03-16 01:01:34 +0700

    Put setup.py as preparation to distribute package

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='pygoogling',
+    version='0.0.1',
+    description='Python library to do google search',
+    long_description=long_description,
+    url='https://github.com/essanpupil/pygoogling',
+    author='Ikhsan Noor Rosyidin',
+    author_email='jakethitam1985@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords='google search python module',
+    py_module=['pygoogling/googling.py'],
+    install_requires=['bs4','requests','html5lib'],
+)
