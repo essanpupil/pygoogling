116abb1298fcd12bf90a6cc6774d741a3b55b601
Ikhsan Noor Rosyidin <jakethitam1985@gmail.com>
2016-03-17 02:11:58 +0700

    Adjust setup.py to adjust with pylint

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
-from setuptools import setup, find_packages
-from codecs import open
+"""Setup script for pygoogling."""
+from codecs import open as open_codec
 from os import path
+from setuptools import setup
 
-here = path.abspath(path.dirname(__file__))
+HERE = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+with open_codec(path.join(HERE, 'README.md'), encoding='utf-8') as f:
+    LONG_DESCRIPTION = f.read()
 
 
 setup(
     name='pygoogling',
     version='0.0.1',
     description='Python library to do google search',
-    long_description=long_description,
+    long_description=LONG_DESCRIPTION,
     url='https://github.com/essanpupil/pygoogling',
     author='Ikhsan Noor Rosyidin',
     author_email='jakethitam1985@gmail.com',
@@ -26,5 +27,5 @@ setup(
     ],
     keywords='google search python module',
     py_modules=['pygoogling.googling'],
-    install_requires=['bs4','requests','html5lib'],
+    install_requires=['bs4', 'requests', 'html5lib'],
 )
